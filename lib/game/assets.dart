9ff8c05dabfb1c0449716b08100d1ca4aba68fa9
Mayur Panchal <mayurpanchal527@gmail.com>
2024-06-25 15:28:24 +0530

    first commit

---
+++ lib/game/assets.dart
@@ -0,0 +1,20 @@
+class Assets {
+  static const backgorund = 'background-night.png';
+  static const ground = 'ground.png';
+  static const clouds = 'clouds.png';
+  static const pipe = 'pipe.png';
+  static const pipeRotated = 'pipe_rotated.png';
+
+  static const birdMidFlap = 'bluebird-midflap.png';
+  static const birdUpFlap = 'bluebird-upflap.png';
+  static const birdDownFlap = 'bluebird-downflap.png';
+
+  static const gameOver = 'assets/images/gameover.png';
+  static const menu = 'assets/images/background-night.png';
+  static const message = 'assets/images/message1.png';
+  static const logo = "assets/images/logo2.png";
+
+  static const flying = 'fly.wav';
+  static const collision = 'collision.wav';
+  static const point = 'point.wav';
+}
